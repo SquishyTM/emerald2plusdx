720a3115341d3b24bf959774109cddf15f1e7d2a
SquishyTM <supercaio4@gmail.com>
2025-11-04 01:03:43 -0300

    I have nuked the full menu start screen. This makes Callie happy, I think.

--- include/overworld.h
+++ include/overworld.h
@@ -171,7 +171,6 @@ bool32 Overworld_RecvKeysFromLinkIsRunning(void);
 bool32 Overworld_SendKeysToLinkIsRunning(void);
 bool32 IsSendingKeysOverCable(void);
 void ClearLinkPlayerObjectEvents(void);
-void CB2_ReturnToFullScreenStartMenu(void);
 bool16 SetTimeOfDay(u16 hours);
 
 // Item Description Headers
